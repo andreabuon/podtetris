aa5b3320830323dc59bdba577fc8cfb72440289a
Andrea Buonaurio <65712954+andreabuon@users.noreply.github.com>
2026-04-22 16:18:01 +0200

    feat(lab): add multiple zones and add topology constraints

--- lab/setup-labelled-cluster.sh
+++ lab/setup-labelled-cluster.sh
@@ -9,7 +9,7 @@ if kind get clusters | grep -q "^$CLUSTER_NAME$"; then
 fi
 
 # Create the cluster
-kind create cluster --config kind-config.yaml
+kind create cluster --config ./kind-config.yaml
 
 # Set the context to the new cluster
 kubectl cluster-info --context kind-$CLUSTER_NAME
@@ -22,10 +22,15 @@ kubectl wait --for=condition=Ready nodes --all --timeout=60s
 # Apply labels to the nodes
 echo ""
 echo "Applying labels to the nodes..."
-kubectl label nodes $CLUSTER_NAME-worker disk=nvme
-kubectl label nodes $CLUSTER_NAME-worker2 disk=ssd
-kubectl label nodes $CLUSTER_NAME-worker3 disk=ssd
-kubectl label nodes $CLUSTER_NAME-worker4 disk=hdd
+## EU region
+### Zone 1
+kubectl label nodes $CLUSTER_NAME-worker disk=nvme zone=eu-1
+kubectl label nodes $CLUSTER_NAME-worker2 disk=ssd zone=eu-1
+kubectl label nodes $CLUSTER_NAME-worker3 disk=hdd zone=eu-1
+### Zone 2
+kubectl label nodes $CLUSTER_NAME-worker4 disk=nvme zone=eu-2
+kubectl label nodes $CLUSTER_NAME-worker5 disk=ssd zone=eu-2
+kubectl label nodes $CLUSTER_NAME-worker6 disk=hdd zone=eu-2
 
 echo ""
 echo "Cluster setup completed."
