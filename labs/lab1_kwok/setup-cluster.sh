a49a769baee03fd6856f940fe72707fdcd217fcb
Andrea Buonaurio <65712954+andreabuon@users.noreply.github.com>
2026-05-05 18:49:12 +0200

    feat(lab): separate deployments manifests

--- labs/lab1_kwok/setup-cluster.sh
+++ labs/lab1_kwok/setup-cluster.sh
@@ -15,4 +15,8 @@ kubectl config use-context kwok-kwok
 helm install kwok-nodes-provisioner charts/kwok-nodes-provisioner/
 
 kubectl apply -f manifests/fake-pods.yaml
-# kubectl apply -f manifests/multiple-deployments.yaml //FIXME right now pods are not scheduled because disk labels are missing
+
+//FIXME right now the following pods are not scheduled because disk labels are missing on the nodes.
+# kubectl apply -f manifests/databases.yaml
+# kubectl apply -f manifests/fake-backups.yaml
+# kubectl apply -f manifests/web-servers.yaml
