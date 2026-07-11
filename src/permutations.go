b9ed8de9ae46d135e09e775d90d563d0918d8e4f
Andrea Buonaurio <65712954+andreabuon@users.noreply.github.com>
2026-07-11 15:14:45 +0200

    refactor(permutations): calculate requests only once during sorting and add nil checks

--- src/permutations.go
+++ src/permutations.go
@@ -15,60 +15,76 @@ var permutationStrategies = map[string]func([]*apiv1.Pod) []*apiv1.Pod{
 }
 
 func sortByCPUDesc(pods []*apiv1.Pod) []*apiv1.Pod {
-	result := make([]*apiv1.Pod, len(pods))
-	copy(result, pods)
-	sort.Slice(result, func(i, j int) bool {
-		reqI, err := getPodCPURequests(result[i])
-		if err != nil {
-			log.Fatalf("Error while sorting pods by CPU usage: %v", err)
-		}
-		reqj, err := getPodCPURequests(result[j])
-		if err != nil {
-			log.Fatalf("Error while sorting pods by CPU usage: %v", err)
-		}
+	if pods == nil {
+		return nil
+	}
 
-		return reqI > reqj
+	requests := make(map[*apiv1.Pod]int64, len(pods))
+	for _, p := range pods {
+		req, err := getPodCPURequests(p)
+		if err != nil {
+			log.Printf("Warning: could not get CPU request for pod %s: %v", p.Name, err)
+		}
+		requests[p] = req
+	}
+
+	sort.SliceStable(pods, func(i, j int) bool {
+		return requests[pods[i]] > requests[pods[j]]
 	})
-	return result
+
+	return pods
 }
 
 func sortByMemoryDesc(pods []*apiv1.Pod) []*apiv1.Pod {
-	result := make([]*apiv1.Pod, len(pods))
-	copy(result, pods)
-	sort.Slice(result, func(i, j int) bool {
-		reqI, err := getPodMemoryRequests(result[i])
-		if err != nil {
-			log.Fatalf("Error while sorting pods by memory usage: %v", err)
-		}
-		reqj, err := getPodMemoryRequests(result[j])
-		if err != nil {
-			log.Fatalf("Error while sorting pods by memory usage: %v", err)
-		}
+	if pods == nil {
+		return nil
+	}
 
-		return reqI > reqj
+	requests := make(map[*apiv1.Pod]int64, len(pods))
+	for _, p := range pods {
+		req, err := getPodMemoryRequests(p)
+		if err != nil {
+			log.Printf("Warning: could not get memory request for pod %s: %v", p.Name, err)
+		}
+		requests[p] = req
+	}
+
+	sort.SliceStable(pods, func(i, j int) bool {
+		return requests[pods[i]] > requests[pods[j]]
 	})
-	return result
+
+	return pods
 }
 
 func shufflePods(pods []*apiv1.Pod) []*apiv1.Pod {
-	result := make([]*apiv1.Pod, len(pods))
-	copy(result, pods)
-	rand.Shuffle(len(result), func(i, j int) {
-		result[i], result[j] = result[j], result[i]
+	if pods == nil {
+		return nil
+	}
+
+	rand.Shuffle(len(pods), func(i, j int) {
+		pods[i], pods[j] = pods[j], pods[i]
 	})
-	return result
+
+	return pods
 }
 
 func generatePermutations(evictedPods []*apiv1.Pod, enabledStrategies []string) [][]*apiv1.Pod {
-	var generatedPermutations [][]*apiv1.Pod
+	if evictedPods == nil {
+		return nil
+	}
+
+	var generatedPermutations [][]*apiv1.Pod = make([][]*apiv1.Pod, 0, len(enabledStrategies))
 
 	for _, strategy := range enabledStrategies {
-		strategyFn, ok := permutationStrategies[strategy]
+		strategyFunc, ok := permutationStrategies[strategy]
 		if !ok {
 			log.Printf("Warning: unknown permutation strategy %q, skipping", strategy)
 			continue
 		}
-		generatedPermutations = append(generatedPermutations, strategyFn(evictedPods))
+
+		podsCopy := make([]*apiv1.Pod, len(evictedPods))
+		copy(podsCopy, evictedPods)
+		generatedPermutations = append(generatedPermutations, strategyFunc(podsCopy))
 	}
 
 	return generatedPermutations
