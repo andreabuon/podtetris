d499b4143c912451531ec366f373650a2f161200
Andrea Buonaurio <65712954+andreabuon@users.noreply.github.com>
2026-08-27 13:40:24 +0200

    fix(controller): fix log message

--- src/evictor/internal/controller/podmove_controller.go
+++ src/evictor/internal/controller/podmove_controller.go
@@ -105,7 +105,7 @@ func (r *PodMoveReconciler) Reconcile(ctx context.Context, req ctrl.Request) (re
 			}
 			return ctrl.Result{}, nil
 		} else {
-			log.Info("The replacement pod phase is not 'Running' yet. Checking again in", verifyRunningInterval)
+			log.Info("The replacement pod phase is not 'Running' yet. Checking again in", "interval", verifyRunningInterval)
 			return ctrl.Result{RequeueAfter: verifyRunningInterval}, nil
 		}
 	}
